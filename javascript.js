f232735532b975da8dd2d86e029140bec8357f41
wiekusviljoen <wiekusviljoen@gmail.com>
2022-03-19 19:39:33 +0200

    commit on master

--- javascript.js
+++ javascript.js
@@ -1,4 +1,11 @@
-
+document.getElementById("clickMe").onclick = function (){
+  alert('helo');
+};
+var el = document.getElementById("clickMe");
+if (el.addEventListener)
+el.addEventListener("click", doFunction, false);
+else if (el.attachEvent)
+el.attachEvent('onclick',doFunction);
 
 
  
