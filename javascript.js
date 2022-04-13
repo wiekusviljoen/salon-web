43d71808927f4a6a5cb4a865dda08e88f4abd716
wiekusviljoen <wiekusviljoen@gmail.com>
2022-04-13 11:04:01 +0200

    commit on master

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
+name = prompt("Please enter your name");
+alert("Welcome " + name + "!");
+var a = 10;
+a = true;
+alert(a);
+a = "Hello world";
+alert(a);
+function runMe() {
+  var un = document.getElementById("name").value;
+  var pw = document.getElementById("pass").value;
 
+  alert("Username: " + un + " Password: " + pw);
+  alert(document.getElementById("output").innerHTML);
+  var a = "change this ";
+  document.getElementById("output").value.innerHTML = a;
+  document.getElementById("btn1").value = "submit";
+}
